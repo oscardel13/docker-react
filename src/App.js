9777553c88ba6e272186454a9cb8162f1caa5ac8
Oscar Delgado <oscardel413@gmail.com>
2023-03-06 16:36:44 -0700

    test workflow

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello react! I am Oscar! Sike I'm you
+          Hello react! I am Oscar!
         </p>
         <a
           className="App-link"
